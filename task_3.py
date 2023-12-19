f27560d43d78d6620d7e098cc6db47c015e5c061
Mirzoeva0513 <145575731+Mirzoeva0513@users.noreply.github.com>
2023-12-19 19:28:02 +0300

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -34,5 +34,31 @@ https://ru.wikipedia.org/wiki/Сочетание
 
 def get_combinations(s: str, n: int):
     ### начало решения
-   
+   def get_combinations(s, n):
+    if n == 0 or len(s) == 0:
+        return []
+    elif n == 1:
+        return list(s)
+    else:
+        combinations = []
+        for i in range(len(s)):
+            combinations.append(s[i])
+            partials = get_combinations(s[i+1:], n-1)
+            for partial in partials:
+                combinations.append(s[i] + partial)
+                combinations.append(partial + s[i])
+        if n > 2:
+            combinations = list(set([perm for c in combinations for perm in get_permutations(c)]))
+        return combinations
+
+def get_permutations(s):
+    if len(s) == 1:
+        return [s]
+    else:
+        perms = []
+        for i in range(len(s)):
+            rest = s[:i] + s[i+1:]
+            for p in get_permutations(rest):
+                perms.append(s[i] + p)
+        return perms
     ### конец решения
